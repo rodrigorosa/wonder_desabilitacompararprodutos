0f87290f2a4c26ca2a0e5a8e7a7eda696ea2c128
Rodrigo Rosa <rodrigo@wonder.com.br>
2017-01-27 17:11:07 -0200

    Adicionados ao repositório

---
+++ app/code/local/Wonder/DesabilitaCompararProdutos/Helper/Compare.php
@@ -0,0 +1,9 @@
+<?php
+
+class Wonder_DesabilitaCompararProdutos_Helper_Compare extends Mage_Catalog_Helper_Product_Compare
+{
+  public function getAddUrl($product)
+  {
+    return '';
+  }
+}
---
+++ app/code/local/Wonder/DesabilitaCompararProdutos/Helper/Data.php
@@ -0,0 +1,6 @@
+<?php
+
+class Wonder_DesabilitaCompararProdutos_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
